9a6252fabf2f3f13e3133f34667cd15c331127fd
Alec Combe <aleccombe@mail.weber.edu>
2020-05-08 16:12:04 -0600

    added some code to image_scan.py code currently doesn't work but should once python-shell is implemented

--- tensorflow/image_scan.py
+++ tensorflow/image_scan.py
@@ -1,5 +1,11 @@
 import sys
+from nudenet import NudeClassifier
+
+classifier = NudeClassifier()
+
+#classifier.classify(sys.argv[1])
 
 #use python shell module so we can include packages
 print("Output from Python")
-print("url: " + sys.argv[1])
+print(classifier.classify(sys.argv[1]))
+#print("image is : " + {'path_to_nude_image': {'safe': 5.8822202e-08, 'unsafe': 1.0}})
